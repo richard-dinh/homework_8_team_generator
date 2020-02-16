b724290a5ee194e45c16b35586d5b8bb2bd06edb
richard-dinh <richarddinh@gmail.com>
2020-02-15 21:13:58 -0800

    create prompt for team size

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const prompt = require('inquirer').createPromptModule()
 const Employee = require("./lib/Employee.js")
+const Engineer = require('./lib/Engineer.js')
+const Intern = require('./lib/Intern.js')
+const Manager = require('./lib/Manager.js')
+
+const init = async function (){
+  let {size} = await prompt([
+    {
+      type: 'input',
+      name: 'size',
+      message: 'Welcome to the Team Builder! How big is your team?'
+    }
+  ])
+  console.log(size)
+}
+
+init()
