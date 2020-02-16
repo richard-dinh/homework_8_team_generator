5bb42c3f72941799405b107a4d9c22e7ba2f262a
richard-dinh <richarddinh@gmail.com>
2020-02-15 21:32:24 -0800

    wrote code to get manager info

--- app.js
+++ app.js
@@ -4,15 +4,56 @@ const Engineer = require('./lib/Engineer.js')
 const Intern = require('./lib/Intern.js')
 const Manager = require('./lib/Manager.js')
 
-const init = async function (){
-  let {size} = await prompt([
+let team = []
+let person
+const init = async _ =>{
+  let {managerName} = await prompt([
     {
       type: 'input',
-      name: 'size',
-      message: 'Welcome to the Team Builder! How big is your team?'
+      name: 'managerName',
+      message: 'Welcome to the Team Builder! What is the name of the team manager?'
     }
   ])
-  console.log(size)
+  console.log(managerName)
+  let {managerID, managerEmail, managerPhone} = await prompt([
+    {
+      type: 'number',
+      name: 'managerID',
+      message: `What is ${managerName}'s ID number?`
+    },
+    {
+      type: 'input',
+      name: 'managerEmail',
+      message: `What is ${managerName}'s email address?`
+    },
+    //validate phone number by inquirer
+    // {
+    //   type: 'input',
+    //   name: 'managerPhone',
+    //   message: "What's your phone number?",
+    //   validate: function (value) {
+    //     var pass = value.match(
+    //       /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+    //     )
+    //     if (pass) {
+    //       return true
+    //     }
+
+    //     return 'Please enter a valid phone number'
+    //   }
+    // }
+    {
+      type: 'input',
+      name: 'managerPhone',
+      message: `What is ${managerName}'s phone number?`
+    }
+  ])
+  person = new Manager(managerName, managerID, managerEmail, managerPhone)
+  console.log(team)
+  
+  
 }
+
+
 
 init()
